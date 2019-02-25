eb901239d1f809a8fb34005bb1bac6b06406dbc8
jacobmorra <jacob.morra@uoit.net>
2019-02-25 03:14:11 -0500

    update index with conn db

--- index.php
+++ index.php
@@ -48,6 +48,8 @@ $client->messages->create(
 
 #PART 3 - USE GOOGLE MAPS API TO GET LOCATION
 ?>
+
+<!--
 <div id="googleMap" style="width:100%;height:400px;"></div>
 
 <script>
@@ -61,6 +63,39 @@ var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 </script>
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCTyb4-Pduzn2umNzIUER9joB5EZdHoEs&callback=myMap"></script>
+-->
+
+<!-- PART 4 - USE GOOGLE VISION TO CLASSIFY PICS-->
+<?php
+# includes the autoloader for libraries installed with composer
+require __DIR__ . '/vendor/autoload.php';
+
+# imports the Google Cloud client library
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+
+# instantiates a client
+$imageAnnotator = new ImageAnnotatorClient();
+
+# the name of the image file to annotate
+$fileName = 'test/data/wakeupcat.jpg';
+
+# prepare the image to be annotated
+$image = file_get_contents($fileName);
+
+# performs label detection on the image file
+$response = $imageAnnotator->labelDetection($image);
+$labels = $response->getLabelAnnotations();
+
+if ($labels) {
+    echo("Labels:" . PHP_EOL);
+    foreach ($labels as $label) {
+        echo($label->getDescription() . PHP_EOL);
+    }
+} else {
+    echo('No label found' . PHP_EOL);
+}
+?>
+
 
 
 <?php
