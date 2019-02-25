4dbd8fbb2cb5de0422078f05e46ab2c92d4f4df3
jacobmorra <jacob.morra@uoit.net>
2019-02-25 13:38:44 -0500

    update index with conn db

--- index.php
+++ index.php
@@ -10,8 +10,9 @@ $vision = new VisionClient(['keyFile' => json_decode(file_get_contents("cloudvis
 
 $familyPhotoResource = fopen('https://www.photonottingham.co.uk/wp-content/uploads/2018/07/photonottingham-family-contemporary-square.jpg', 'r') or die('FUCK KEVIN');
 
-/*$image = $vision->image($familyPhotoResource, ['FACE_DETECTION']);
+$image = $vision->image($familyPhotoResource, ['FACE_DETECTION']);
 
+/*
 $result = $vision->annotate($image);
 
 echo "hahahaha4";
