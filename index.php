2a1137df5d5e4df97b308b455494e8803192837c
jacobmorra <jacob.morra@uoit.net>
2019-02-25 16:20:18 -0500

    update

--- index.php
+++ index.php
@@ -1,39 +1,23 @@
 <?php
 
-# includes the autoloader for libraries installed with composer
-require __DIR__ . '/vendor/autoload.php';
-
-# imports the Google Cloud client library
-use Google\Cloud\Vision\V1\ImageAnnotatorClient;
-
-# instantiates a client
-$imageAnnotator = new ImageAnnotatorClient();
-
-# the name of the image file to annotate
-$fileName = 'test/data/wakeupcat.jpg';
-
-# prepare the image to be annotated
-$image = file_get_contents($fileName);
-
-# performs label detection on the image file
-$response = $imageAnnotator->labelDetection($image);
-$labels = $response->getLabelAnnotations();
-
-if ($labels) {
-    echo("Labels:" . PHP_EOL);
-    foreach ($labels as $label) {
-        echo($label->getDescription() . PHP_EOL);
-    }
-} else {
-    echo('No label found' . PHP_EOL);
-}
-/*
-session_start();
-
 require "vendor/autoload.php";
 
 use Google\Cloud\Vision\VisionClient;
 
+use Google\Cloud\Core\ServiceBuilder;
+
+$gcloud = new ServiceBuilder([
+    'keyFilePath' => 'cloudviskey.json',
+    'projectId' => 'myProject'
+]);
+
+$storage = $gcloud->storage();
+
+$bucket = $storage->bucket('myBucket');
+
+echo "good";
+
+/*
 $vision = new VisionClient(['keyFile' => json_decode(file_get_contents("cloudviskey.json"),true)]);
 
 $familyPhotoResource = fopen('fampic.jpg', 'r');
