9f1ab7694fc1a58aae24193e7901784cec87a9a6
jacobmorra <jacob.morra@uoit.net>
2019-02-26 12:39:31 -0500

    update

--- script.php
+++ script.php
@@ -55,111 +55,98 @@ $client->messages->create(
 
 
 <!DOCTYPE html>
+<!-- PART 3 - GOOGLE MAPS API IMPLEMENTATION - TESTED AND WORKS -->
 <html lang="en">
-<head>
-	<title>Come Find Me</title>
-	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-	<style>
-		  /* Always set the map height explicitly to define the size of the div
-		   * element that contains the map. */
-		  #map {
-		    height: 600px;
-		  }
-		  /* Optional: Makes the sample page fill the window. */
-		  html, body {
-		    height: 100%;
-		    margin: 0;
-		    padding: 0;
-		  }
-		  #floating-panel {
-		    position: absolute;
-		    top: 10px;
-		    left: 25%;
-		    z-index: 5;
-		    background-color: #fff;
-		    padding: 5px;
-		    border: 1px solid #999;
-		    text-align: center;
-		    font-family: 'Roboto','sans-serif';
-		    line-height: 30px;
-		    padding-left: 10px;
-		  }
-	</style>
-</head>
-<body>
-<div class="jumbotron text-center">
-	  <h1>Come Find Me</h1> 
-</div>
-<div class="container">
-		<div class="row">
-<div id="floating-panel">
-    <b>Start: </b>
-    <select id="start">
-      <option value=""></option>
-      <option value=<?php echo $_POST['address'] ?>><?php echo $_POST['address'] ?></option>
-    </select>
-    <b>End: </b>
-    <select id="end">
-      <option value="2000 Simcoe Street North, ON">2000 Simcoe Street North, ON</option>
-    </select>
-    </div>
-    <div id="map"></div>
-    <script>
-      function initMap() {
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 7,
-          center: {lat: 41.85, lng: -87.65}
-        });
-        directionsDisplay.setMap(map);
-
-        var onChangeHandler = function() {
-          calculateAndDisplayRoute(directionsService, directionsDisplay);
-        };
-        document.getElementById('start').addEventListener('change', onChangeHandler);
-        document.getElementById('end').addEventListener('change', onChangeHandler);
-      }
-
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        directionsService.route({
-          origin: document.getElementById('start').value,
-          destination: '2000 Simcoe Street North, ON',
-          travelMode: 'DRIVING'
-        }, function(response, status) {
-          if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
-    </script>
-	<script async defer 
-	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCTyb4-Pduzn2umNzIUER9joB5EZdHoEs&callback=initMap">
-	</script>
-
+	<head>
+		<title>Come Find Me</title>
+		<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+		<style>
+			  /* Always set the map height explicitly to define the size of the div
+			   * element that contains the map. */
+			  #map {
+				height: 600px;
+			  }
+			  /* Optional: Makes the sample page fill the window. */
+			  html, body {
+				height: 100%;
+				margin: 0;
+				padding: 0;
+			  }
+			  #floating-panel {
+				position: absolute;
+				top: 100px;
+				left: 25%;
+				z-index: 5;
+				background-color: #fff;
+				padding: 5px;
+				border: 1px solid #999;
+				text-align: center;
+				font-family: 'Roboto','sans-serif';
+				line-height: 30px;
+				padding-left: 10px;
+			  }
+		</style>
+	</head>
+	<body>
+		<div class="jumbotron text-center">
+		  <h1>Come Find Me</h1> 
 		</div>
-</div>
-    
-  </body>
+		<div class="container">
+			<div class="row">
+				<div id="floating-panel">
+				<b>Start: </b>
+				<select id="start">
+					<option value=""></option>
+					<option value=<?php echo $_POST['address'] ?>><?php echo $_POST['address'] ?>
+					</option>
+				</select>
+				<b>End: </b>
+					<select id="end">
+					<option value="2000 Simcoe Street North, ON">2000 Simcoe Street North, ON</option>
+					</select>
+			</div>
+			<div id="map"></div>
+				<script>
+				  function initMap() {
+					var directionsService = new google.maps.DirectionsService;
+					var directionsDisplay = new google.maps.DirectionsRenderer;
+					var map = new google.maps.Map(document.getElementById('map'), {
+					  zoom: 7,
+					  center: {lat: 41.85, lng: -87.65}
+					});
+					directionsDisplay.setMap(map);
 
-<body>
-	<div class="jumbotron text-center">
-	  <h1>Come Find Me</h1> 
-	</div>
-	<div class="container">
-		<div class="row">
-			<form action="script.php" method="post">
-			Address: <input type="text" name="address" id="address"><br>
-			<input type="submit">
-			</form>	
+					var onChangeHandler = function() {
+					  calculateAndDisplayRoute(directionsService, directionsDisplay);
+					};
+					document.getElementById('start').addEventListener('change', onChangeHandler);
+					document.getElementById('end').addEventListener('change', onChangeHandler);
+				  }
+
+				  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+					directionsService.route({
+					  origin: document.getElementById('start').value,
+					  destination: '2000 Simcoe Street North, ON',
+					  travelMode: 'DRIVING'
+					}, function(response, status) {
+					  if (status === 'OK') {
+						directionsDisplay.setDirections(response);
+					  } else {
+						window.alert('Directions request failed due to ' + status);
+					  }
+					});
+				  }
+				</script>
+				<script async defer 
+				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCTyb4-Pduzn2umNzIUER9joB5EZdHoEs&callback=initMap">
+				</script>
+
+			</div>
 		</div>
-	</div>
-</body>
+	</body>
 </html>
