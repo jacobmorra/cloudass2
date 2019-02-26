3daf03f9ada46c099705e219c562bc656a04788b
jacobmorra <jacob.morra@uoit.net>
2019-02-26 05:06:21 -0500

    update

--- script.php
+++ script.php
@@ -1,5 +1,24 @@
 <?php
 
-
-
 ?>
+
+<html>
+    <head><meta charset="UTF-8"></head>
+	<body>	
+	<h1>Come Find Me</h1>
+	<!--PART 3 - USE GOOGLE MAPS API TO GET LOCATION-->
+	<div id="googleMap" style="width:100%;height:400px;"></div>
+
+	<script>
+	function myMap() {
+	var mapProp= {
+	  center:new google.maps.LatLng(51.508742,-0.120850),
+	  zoom:5,
+	};
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	}
+	</script>
+
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCTyb4-Pduzn2umNzIUER9joB5EZdHoEs&callback=myMap"></script>
+	</body>
+</html>
