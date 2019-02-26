2988d73d88430bf6d8fc8e4a755b67a3e795807a
jacobmorra <jacob.morra@uoit.net>
2019-02-26 11:44:37 -0500

    update

--- script.php
+++ script.php
@@ -44,6 +44,7 @@ $client->messages->create(
 
 <!DOCTYPE html>
 <html>
+<!--PART 3 - USE GOOGLE MAPS API TO GET LOCATION - TESTED WORKS-->
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
@@ -76,6 +77,7 @@ $client->messages->create(
     </style>
   </head>
   <body>
+<h1>Come Find Me</h1>
     <div id="floating-panel">
     <b>Start: </b>
     <select id="start">
@@ -124,28 +126,3 @@ $client->messages->create(
 	</script>
   </body>
 </html>
-
-<!--
-<html>
-    <head><meta charset="UTF-8"></head>
-	<body>	
-	<h1>Come Find Me</h1>
--->
-	<!--PART 3 - USE GOOGLE MAPS API TO GET LOCATION-->
-<!--
-	<div id="googleMap" style="width:100%;height:400px;"></div>
-
-	<script>
-	function myMap() {
-	var mapProp= {
-	  center:new google.maps.LatLng(51.508742,-0.120850),
-	  zoom:5,
-	};
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	}
-	</script>
-
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCTyb4-Pduzn2umNzIUER9joB5EZdHoEs&callback=myMap"></script>
-	</body>
-</html>
--->
